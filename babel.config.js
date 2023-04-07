7aa03d403f5e71e88a0e243a9c2157a5da1119cf
VASILI HRYB <vasilihryb@MacBook-Pro-2.local>
2023-04-07 13:36:12 +0300

    update project structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
+  plugins: [
+    [
+      'module-resolver',
+      {
+        alias: {
+          '@': './src',
+        },
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+        root: ['./src'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
   presets: ['module:metro-react-native-babel-preset'],
 };
