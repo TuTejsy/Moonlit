f2bd9cabfe1a0823ea4419c4d5fb8dd6b73da881
VASILI HRYB <vasilihryb@MacBook-Pro-5.local>
2024-01-22 22:39:32 +0300

    fix bug with scroll

--- src/hooks/useAnimatedScrollHandlerValue.ts
+++ src/hooks/useAnimatedScrollHandlerValue.ts
@@ -1,11 +1,17 @@
 import { useAnimatedScrollHandler, useSharedValue } from 'react-native-reanimated';
 
+import { LARGE_TITLE_HEIGHT } from '@/components/Headers/ScreenHeader/ScreenHeader.constants';
+
 export const useAnimatedScrollHandlerValue = () => {
   const scrollPositionSharedValue = useSharedValue(0);
 
   const handleAnimatedScroll = useAnimatedScrollHandler({
     onScroll: (event) => {
-      scrollPositionSharedValue.value = event.contentOffset.y;
+      const { contentOffset, contentSize, layoutMeasurement } = event;
+
+      if (contentSize.height - layoutMeasurement.height > LARGE_TITLE_HEIGHT) {
+        scrollPositionSharedValue.value = contentOffset.y;
+      }
     },
   });
 
