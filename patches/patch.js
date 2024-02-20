7c0b2a4d19e993d4127fc3e0c80cdb55fc45b74e
VASILI HRYB <vasilihryb@MacBook-Pro-5.local>
2024-02-20 16:05:25 +0400

    add jwt authorization to request headers

--- patches/patch.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-const fileData = fs.readFileSync('./patches/react-native/RCTPullToRefreshViewComponentView.mm');
-const reactNativePullToRefreshPath = './node_modules/react-native/React/Fabric/Mounting/ComponentViews/ScrollView/RCTPullToRefreshViewComponentView.mm';
-fs.writeFileSync(reactNativePullToRefreshPath, fileData);
-
-console.log('patch succeed');
--- src/services/networkClient/networkClient.ts
+++ src/services/networkClient/networkClient.ts
@@ -1,6 +1,15 @@
 /* eslint-disable dot-notation */
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import qs from 'qs';
+import { sign } from 'react-native-pure-jwt';
+
+import { JWT_PAYLOAD, JWT_SECRET } from '@/constants/auth';
+
+let jwt: string | undefined;
+
+sign({ sub: JWT_PAYLOAD }, JWT_SECRET, { alg: 'HS256' }).then((token) => {
+  jwt = token;
+});
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
@@ -15,6 +24,11 @@ export class NetworkClient {
     this.instance = axios.create({
       ...config,
       paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'comma', encode: false }),
+    });
+
+    this.instance.interceptors.request.use((request) => {
+      request.headers.Authorization = jwt;
+      return request;
     });
   }
 }
