fdd49f787d025026fe11314e339fb3123af83f18
tutejsy <tutejsy@protonmail.com>
2024-02-29 18:37:48 +0400

    move token to constant

--- src/services/networkClient/networkClient.ts
+++ src/services/networkClient/networkClient.ts
@@ -1,20 +1,14 @@
 /* eslint-disable dot-notation */
 import axios, { AxiosInstance, AxiosRequestConfig } from 'axios';
 import qs from 'qs';
-import { sign } from 'react-native-pure-jwt';
 
-import { JWT_PAYLOAD, JWT_SECRET } from '@/constants/auth';
-
-let jwt: string | undefined;
-
-sign({ sub: JWT_PAYLOAD }, JWT_SECRET, { alg: 'HS256' }).then((token) => {
-  jwt = token;
-});
+import { JWT_TOKEN } from '@/constants/auth';
 
 axios.defaults.headers.common['Content-Type'] = 'application/json';
 axios.defaults.headers.common['Accept'] = 'application/json';
 axios.defaults.headers.common['Cache-Control'] = 'no-cache';
 axios.defaults.headers.common['pragma'] = 'no-cache';
+axios.defaults.headers.common['Authorization'] = JWT_TOKEN;
 axios.defaults.withCredentials = false;
 
 export class NetworkClient {
@@ -24,11 +18,6 @@ export class NetworkClient {
     this.instance = axios.create({
       ...config,
       paramsSerializer: (params) => qs.stringify(params, { arrayFormat: 'comma', encode: false }),
-    });
-
-    this.instance.interceptors.request.use((request) => {
-      request.headers.Authorization = jwt;
-      return request;
     });
   }
 }
