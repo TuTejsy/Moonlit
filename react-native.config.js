7ab7d71f406f0dfd0d1e3a62a1d746c62ecda5e5
tutejsy <tutejsy@protonmail.com>
2024-03-24 20:22:15 +0300

    update android logo

--- react-native.config.js
+++ react-native.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   assets: ['./src/assets/fonts'],
-  dependencies: {
-    ...(process.env.NO_FLIPPER
-      ? {
-        'react-native-flipper': {
-          platforms: {
-            ios: null,
-          },
-        },
-      }
-      : {}),
-  },
   project: {
     android: {},
     ios: {},
