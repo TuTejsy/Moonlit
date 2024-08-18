aa07c55a8652d7dfc6ba846853ea9eb9d62f6b43
tutejsy <tutejsy@protonmail.com>
2024-08-18 16:02:21 +0300

    refactor WINDOW_MAX_WIDTH and SCREEN_MAX_WIDTH to handle dimensions change

--- src/components/Lists/MoreTalesComingFooter/MoreTalesComingFooter.styles.ts
+++ src/components/Lists/MoreTalesComingFooter/MoreTalesComingFooter.styles.ts
@@ -1,21 +1,28 @@
 import { StyleSheet } from 'react-native';
 
-import { SCREEN_MAX_WIDTH, WINDOW_WIDTH } from '@/constants/layout';
 import { TAB_BAR_STORY_PLAYER_HEIGHT } from '@/constants/sizes';
 import { MakeStylesProps } from '@/hooks/theme/useMakeStyles';
 
-export const makeStyles = ({ colors, dh, dw, fonts, insets }: MakeStylesProps) =>
+export const makeStyles = ({
+  colors,
+  dh,
+  dw,
+  fonts,
+  insets,
+  windowMaxWidth,
+  windowWidth,
+}: MakeStylesProps) =>
   StyleSheet.create({
     backgroundImage: {
       height: dw(597),
       left: 0,
       position: 'absolute',
       top: 0,
-      width: WINDOW_WIDTH,
+      width: windowWidth,
     },
     button: {
       marginTop: dh(20),
-      width: dw(213, SCREEN_MAX_WIDTH),
+      width: dw(213, windowMaxWidth),
     },
     container: {
       alignItems: 'center',
@@ -23,12 +30,12 @@ export const makeStyles = ({ colors, dh, dw, fonts, insets }: MakeStylesProps) =
       marginTop: 40,
       paddingBottom: insets.bottom + TAB_BAR_STORY_PLAYER_HEIGHT,
       position: 'relative',
-      width: WINDOW_WIDTH,
+      width: windowWidth,
     },
     logoImage: {
-      height: dw(78, SCREEN_MAX_WIDTH),
+      height: dw(78, windowMaxWidth),
       marginTop: dw(64),
-      width: dw(88, SCREEN_MAX_WIDTH),
+      width: dw(88, windowMaxWidth),
     },
     title: {
       ...fonts.size_24,
--- src/constants/layout.ts
+++ src/constants/layout.ts
@@ -4,13 +4,3 @@ export const { height: WINDOW_HEIGHT, width: WINDOW_WIDTH } = Dimensions.get('wi
 export const { height: SCREEN_HEIGHT, width: SCREEN_WIDTH } = Dimensions.get('screen');
 
 export const [DESIGN_WIDTH, DESIGN_HEIGHT] = [375, 812];
-
-export const SCREEN_MAX_WIDTH = Math.max(
-  (DESIGN_WIDTH / DESIGN_HEIGHT) * SCREEN_HEIGHT,
-  DESIGN_WIDTH,
-);
-
-export const WINDOW_MAX_WIDTH = Math.max(
-  (DESIGN_WIDTH / DESIGN_HEIGHT) * WINDOW_HEIGHT,
-  DESIGN_WIDTH,
-);
